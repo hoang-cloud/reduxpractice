ce2657d92e73fc26c70348d3882eb2abd168ee76
hoang-cloud <hoangdl201096@gmail.com>
2021-01-01 20:38:13 +0700

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,8 @@
 import React from 'react'
 import '../layout/layout.css'
 import {connect} from 'react-redux'
-import {createStore} from 'redux'
 
-const addMessage = (msg) =>{
-    return ({
-        type: 'ADD_TODO',
-        msg: msg
-    })
-}
 
-const reducer = (state = [], action) => {
-    switch(action.type){
-      case 'ADD_TODO':
-        return state.concat(action.msg)
-      default:
-        return state
-    }
-}
-
-console.log(store.dispatch(addMessage("X")))
-
-const store = createStore(reducer)
 
 class App extends React.Component {
     constructor(props){
@@ -52,21 +33,16 @@ class App extends React.Component {
                     </div>
                     <div className="ButtonWrapper">
                     <button className="Button" onClick={this.handleSubmit}>Add your todo</button>
+                    {console.log(this.props.todos)}
                     </div>
                 </div>
             </div>
         )
     }
 }
+
 const mapStateToProps = (state) => {
-    return {todos: state}
+    return {todos : state}
 }
-const mapDispatchToProps = (dispatch) => {
-    return {
-            addToDo: (msg) => 
-            dispatch(addMessage(msg))
-        }
-    }
 
-
-export default connect(mapStateToProps,mapDispatchToProps)(App)
+export default connect(mapStateToProps)(App)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,21 @@ import * as React from "react"
 import App from '../components/App'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+
 const addMessage = (msg) =>{
-    return ({
-        type: 'ADD_MESSAGE',
-        msg: msg
-    })
+  return ({
+      type: 'ADD_TODO',
+      msg: msg
+  })
 }
 
 const reducer = (state = [], action) => {
-    switch(action.type){
-      case 'ADD_MESSAGE':
-        return state.concat(action.msg)
-      default:
-        return state
-    }
+  switch(action.type){
+    case 'ADD_TODO':
+      return state.concat(action.msg)
+    default:
+      return state
+  }
 }
 
 const store = createStore(reducer)
